d5437f0b53b0f17d4375ae76a4d02ce00c96bed7
Abhishek Anand Amralkar <abhishekanand1984@gmail.com>
2019-01-15 20:02:28 +0530

    adding my hacks

---
+++ lisp/init-local.el
@@ -0,0 +1,19 @@
+;; Overriding the default theme
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+
+;; Overriding the default fonts
+(set-frame-font "Inconsolata 16" nil t)
+
+(setq cljr-inject-dependencies-at-jack-in nil)
+
+
+
+;;(add-to-list 'load-path "~/.emacs.d/rust-mode/")
+;;(autoload 'rust-mode "rust-mode" nil t)
+;;(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+(setq rust-format-on-save t)
+
+(provide 'init-local)
